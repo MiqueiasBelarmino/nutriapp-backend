8f7fe38bd4642f2e75cc9fb8cfddcd860b76243a
Miquéias Belarmino <miqueiascostabac@gmail.com>
2025-07-15 10:53:55 -0300

    feat: add users crud and auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '../users/users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService,
+    ) { }
+
+    async validateUser(email: string, password: string) {
+        const user = await this.usersService.findByEmail(email);
+
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            throw new UnauthorizedException('Credenciais inválidas');
+        }
+
+        return user;
+    }
+
+    async login(email: string, password: string) {
+        const user = await this.validateUser(email, password);
+
+        const payload = { sub: user.id, email: user.email };
+        const token = this.jwtService.sign(payload);
+
+        return {
+            access_token: token,
+            user: {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+            },
+        };
+    }
+}
