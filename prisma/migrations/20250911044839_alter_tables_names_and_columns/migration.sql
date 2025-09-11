57f7cd13a5a67bafd8f514a827deba8b1e8ec724
Miquéias Belarmino <miqueiascostabac@gmail.com>
2025-09-11 01:52:54 -0300

    chore (database): change tables names and columns

---
+++ prisma/migrations/20250911044839_alter_tables_names_and_columns/migration.sql
@@ -0,0 +1,107 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Consultation` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `MealPlan` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Patient` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "public"."Consultation" DROP CONSTRAINT "Consultation_patientId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "public"."MealPlan" DROP CONSTRAINT "MealPlan_patientId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "public"."Patient" DROP CONSTRAINT "Patient_userId_fkey";
+
+-- DropTable
+DROP TABLE "public"."Consultation";
+
+-- DropTable
+DROP TABLE "public"."MealPlan";
+
+-- DropTable
+DROP TABLE "public"."Patient";
+
+-- DropTable
+DROP TABLE "public"."User";
+
+-- CreateTable
+CREATE TABLE "public"."users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."patients" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "gender" TEXT NOT NULL,
+    "birth_date" TIMESTAMP(3) NOT NULL,
+    "weight" DOUBLE PRECISION NOT NULL,
+    "height" DOUBLE PRECISION NOT NULL,
+    "userId" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "email" TEXT,
+    "phone" TEXT,
+
+    CONSTRAINT "patients_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."consultations" (
+    "id" TEXT NOT NULL,
+    "patient_id" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "weight" DOUBLE PRECISION NOT NULL,
+    "triceps" DOUBLE PRECISION NOT NULL,
+    "subscapular" DOUBLE PRECISION NOT NULL,
+    "suprailiac" DOUBLE PRECISION NOT NULL,
+    "abdominal" DOUBLE PRECISION NOT NULL,
+    "waist_circ" DOUBLE PRECISION NOT NULL,
+    "abdomen_circ" DOUBLE PRECISION NOT NULL,
+    "fat_formula" TEXT NOT NULL,
+    "fat_percent" DOUBLE PRECISION NOT NULL,
+    "lean_mass" DOUBLE PRECISION NOT NULL,
+    "fat_mass" DOUBLE PRECISION NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "consultations_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."meal_plans" (
+    "id" TEXT NOT NULL,
+    "patient_id" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "content" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "meal_plans_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "public"."users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "patients_email_key" ON "public"."patients"("email");
+
+-- AddForeignKey
+ALTER TABLE "public"."patients" ADD CONSTRAINT "patients_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."consultations" ADD CONSTRAINT "consultations_patient_id_fkey" FOREIGN KEY ("patient_id") REFERENCES "public"."patients"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."meal_plans" ADD CONSTRAINT "meal_plans_patient_id_fkey" FOREIGN KEY ("patient_id") REFERENCES "public"."patients"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
